3235ff1c8a6e12031a943d9436ecaec7aad49661
unknown <danilo.borozan@students.finki.ukim.mk>
2019-03-28 09:52:01 +0100

    printMyIndex

---
+++ program.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void printMyName () {
+cout<<"Danilo Borozan 161025"<<endl;
+	//TODO
+}
+
+void printMyIndex () {
+	cout<<"161025"<<endl;
+}
+
+int main () {
+	//TODO
+}
